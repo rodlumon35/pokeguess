c4d0f2f47b53ccf0451785aa46f9ff3e94f921a2
rodlumon35 <rodlumon35@gmail.com>
2021-07-09 20:49:23 +0100

    added dotenv variables

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
+require('dotenv').config();
 
 // Settings
 app.set('PORT', process.env.PORT || 5000)
